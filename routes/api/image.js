f80858ea7c6fa37dea265363b605a51ba8b7b5b6
Robert Tibbs <tibbsr09@gmail.com>
2019-07-02 21:42:18 -0400

    Port connections established
    Able to establish port connections to 3000 and 3001 Mongodb still not syncing.

--- routes/api/image.js
+++ routes/api/image.js
@@ -1,65 +1,66 @@
-const router = require('express').Router();
-const multer  = require('multer');
-const { mongo, connection } = require('mongoose');
-const Grid = require('gridfs-stream');
-Grid.mongo = mongo;
-var gfs = Grid(connection.db);
-
-// set up connection to db for file storage
-const storage = require('multer-gridfs-storage')({
-   db: connection.db,
-   file: (req, file) => {
-      return {
-         filename: file.originalname
-      }
-   }
-});
-// sets file input to single file
-const singleUpload = multer({ storage: storage }).single('file');
+// const router = require('express').Router();
+// const multer  = require('multer');
+// const { mongo, connection } = require('mongoose');
+// const Grid = require('gridfs-stream');
+// Grid.mongo = mongo;
+// var gfs = Grid(connection.db);
 
 
-router.get('/files/:filename', (req, res) => {
-   gfs.files.find({ filename: req.params.filename }).toArray((err, files) => {
-      if(!files || files.length === 0){
-         return res.status(404).json({
-            message: "Could not find file"
-         });
-      }
+// // set up connection to db for file storage
+// const storage = require('multer-gridfs-storage')({
+//    db: connection.db,
+//    file: (req, file) => {
+//       return {
+//          filename: file.originalname
+//       }
+//    }
+// });
+// // sets file input to single file
+// const singleUpload = multer({ storage: storage }).single('file');
 
-      var readstream = gfs.createReadStream({
-         filename: files[0].filename
-      })
-      res.set('Content-Type', files[0].contentType);
-      return readstream.pipe(res);
-   });
-});
 
-router.get('/files', (req, res) => {
-   gfs.files.find().toArray((err, files) => {
-      if(!files || files.length === 0){
-         return res.status(404).json({
-            message: "Could not find files"
-         });
-      }
-      return res.json(files);
-   });
-});
+// router.get('/files/:filename', (req, res) => {
+//    gfs.files.find({ filename: req.params.filename }).toArray((err, files) => {
+//       if(!files || files.length === 0){
+//          return res.status(404).json({
+//             message: "Could not find file"
+//          });
+//       }
 
-router.post('/files', singleUpload, (req, res) => {
-   if (req.file) {
-      return res.json({
-         success: true,
-         file: req.file
-      });
-   }
-    res.send({ success: false });
-});
+//       var readstream = gfs.createReadStream({
+//          filename: files[0].filename
+//       })
+//       res.set('Content-Type', files[0].contentType);
+//       return readstream.pipe(res);
+//    });
+// });
 
-router.delete('/files/:id', (req, res) => {
-   gfs.remove({ _id: req.params.id }, (err) => {
-      if (err) return res.status(500).json({ success: false })
-      return res.json({ success: true });
-   })
-})
+// router.get('/files', (req, res) => {
+//    gfs.files.find().toArray((err, files) => {
+//       if(!files || files.length === 0){
+//          return res.status(404).json({
+//             message: "Could not find files"
+//          });
+//       }
+//       return res.json(files);
+//    });
+// });
 
-module.exports = router;
+// router.post('/files', singleUpload, (req, res) => {
+//    if (req.file) {
+//       return res.json({
+//          success: true,
+//          file: req.file
+//       });
+//    }
+//     res.send({ success: false });
+// });
+
+// router.delete('/files/:id', (req, res) => {
+//    gfs.remove({ _id: req.params.id }, (err) => {
+//       if (err) return res.status(500).json({ success: false })
+//       return res.json({ success: true });
+//    })
+// })
+
+// module.exports = router;
