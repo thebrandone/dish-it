f5a61cb7fdcf320631cd8663afb7cb3771241d91
Robert Tibbs <tibbsr09@gmail.com>
2019-07-13 13:19:13 -0400

    Update dishController.js
    Fixed find all by creating empty object

--- controllers/dishController.js
+++ controllers/dishController.js
@@ -3,7 +3,7 @@ const db = require("../models");
 module.exports = {
   findAll: function(req, res) {
     db.Dish
-      .find(req.query)
+      .find({})
       .populate("Image")
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
