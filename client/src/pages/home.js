21813063a83a6cde9923057a7bcb14b34577bd83
GregH <43968370+gfhorrocks@users.noreply.github.com>
2019-07-16 19:10:04 -0400

    Fixing routing from greg's messup

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import PostCard from "../components/PostCard";
 import API from "../utils/API";
-import { Container, Button} from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import Foodform from "../components/Foodform"
 import Wrapper from "../components/wrapper"
+import { Link } from "react-router-dom";
+
 // import { Input, TextArea, FormBtn } from "../../components/Form";
 // import Jumbotron from "../../components/Jumbotron";
 // import DeleteBtn from "../../components/DeleteBtn";
@@ -25,12 +27,12 @@ class Home extends Component {
   componentDidMount() {
     this.loadDishes();
   }
-  
+
   // Loads all dishes
   loadDishes = () => {
     API.getDishes()
       .then(res =>
-        this.setState({dishes: Array.from(res.data)})
+        this.setState({ dishes: Array.from(res.data) })
       )
       .catch(err => console.log(err));
   };
@@ -101,9 +103,11 @@ class Home extends Component {
 
     if (!this.state.isloggedIn) {
       return (
-        
+
         <div className="feedWrapper">
-          <Button className="toCompact" href="/compact" size="sm"/>
+          <Link to="/compact">
+            <Button className="toCompact" size="sm" />
+          </Link>
           <Container fluid>
             {this.state.dishes && this.state.dishes.length ? (
               <Container>
@@ -133,8 +137,9 @@ class Home extends Component {
       return (
         <div className="feedWrapper">
 
-          <Button className="toCompact image imgBtn" href="/compact" size="sm"></Button>
-          <Container fluid>
+          <Link to="/compact">
+            <Button className="toCompact" size="sm" />
+          </Link>          <Container fluid>
 
             {this.state.dishes && this.state.dishes.length ? (
               <Container>
