b0aa91e0d6127734131d30e9ece6f72db8b452b4
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-07-15 21:45:38 -0400

    Tags Component

---
+++ client/src/components/Tags/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import './style.css';
+
+class Tags extends React.Component {
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+        tags: props.tags
+      };
+    }
+    
+    removeTag = (i) => {
+      const newTags = [ ...this.state.tags ];
+      newTags.splice(i, 1);
+      this.setState({ tags: newTags });
+    }
+  
+    inputKeyDown = (e) => {
+      const val = e.target.value;
+      if (e.key === 'Enter' && val) {
+        if (this.state.tags.find(tag => tag.toLowerCase() === val.toLowerCase())) {
+          return;
+        }
+        this.setState({ tags: [...this.state.tags, val]});
+        this.tagInput.value = null;
+      } else if (e.key === 'Backspace' && !val) {
+        this.removeTag(this.state.tags.length - 1);
+      }
+    }
+  
+    render() {
+     
+      return (
+        <div className="input-tag">
+          <ul className="input-tag__tags">
+            {this.state.tags.map((tag, i) => (
+              <li key={tag}>
+                {tag}
+                <button type="button" onClick={() => { this.removeTag(i); }}>+</button>
+              </li>
+            ))}
+            <li className="input-tag__tags__input"><input type="text" onKeyDown={this.inputKeyDown} ref={c => { this.tagInput = c; }} /></li>
+          </ul>
+        </div>
+      );
+    }
+  }
+
+  export default Tags;
